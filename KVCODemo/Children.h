7e866f9e1368d4c4cebc95df1c15604329930e94
Jacy Wang <jacywj@gmail.com>
2015-05-13 21:36:56 -0700

    Add Children Class

---
+++ KVCODemo/Children.h
@@ -0,0 +1,16 @@
+//
+//  Children.h
+//  KVCODemo
+//
+//  Created by JIAN WANG on 5/13/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Children : NSObject
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic) NSUInteger age;
+
+@end
---
+++ KVCODemo/Children.m
@@ -0,0 +1,22 @@
+//
+//  Children.m
+//  KVCODemo
+//
+//  Created by JIAN WANG on 5/13/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import "Children.h"
+
+@implementation Children
+
+-(instancetype)init {
+    self = [super self];
+    if (self) {
+        _name = @"";
+        _age = 0;
+    }
+    return self;
+}
+
+@end
